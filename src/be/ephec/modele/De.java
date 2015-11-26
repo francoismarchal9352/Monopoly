57eb3eeab84e83acd9e85bf6897c0ab607519ba9
f.marchal@students.ephec.be <f.marchal@students.ephec.be>
2015-11-26 19:54:18 +0100

    classe De

--- src/be/ephec/modele/De.java
+++ src/be/ephec/modele/De.java
@@ -3,7 +3,12 @@ package be.ephec.modele;
 public class De {
 	private int valeur;
 	
+	public De(){
+	
+	}
+	
 	public int lancerDe(){
+		valeur = (int)(Math.random()*6) + 1;
 		return valeur;
 	}
 }
