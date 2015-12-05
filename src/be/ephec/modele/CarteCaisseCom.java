142abf36d8e9546d1767df252383541894dbedd4
f.marchal@students.ephec.be <f.marchal@students.ephec.be>
2015-12-05 21:49:54 +0100

    methodes carte com (a verifier -- voir commentaires)

--- src/be/ephec/modele/CarteCaisseCom.java
+++ src/be/ephec/modele/CarteCaisseCom.java
@@ -6,40 +6,25 @@ public class CarteCaisseCom extends Carte {
 		super(partie, texte, methodeNum);
 	}
 	
-	public void action(int methodeNum){
+	public void action(int methodeNum){// Ferai ce w-e
 		switch (methodeNum) {
-			case 1:		// Ferai ce w-e
+			case 1: partie.retraitSolde(150,partie.getTabJoueurs().get(partie.getIndexJoueurCourant())); break;		
+			case 2: partie.retraitSolde(150,partie.getTabJoueurs().get(partie.getIndexJoueurCourant())); break;	
+			case 3: partie.ajoutSolde(50,partie.getTabJoueurs().get(partie.getIndexJoueurCourant())); break;
+			case 4: partie.allerA(10); break; // si case depart, pas 200€ 
+			case 5: partie.allerA(1); break;
+			case 6: partie.ajoutSolde(250,partie.getTabJoueurs().get(partie.getIndexJoueurCourant())); break;
+			case 7: partie.retraitSolde(100,partie.getTabJoueurs().get(partie.getIndexJoueurCourant())); break;
+			case 8: // 10 € de chaque joueur
 				break;
-			case 2:
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			case 5:
-				break;
-			case 6:
-				break;
-			case 7:
-				break;
-			case 8:
-				break;
-			case 9:
-				break;
-			case 10:
-				break;
-			case 11:
-				break;
-			case 12:
-				break;
-			case 13:
-				break;
-			case 14:
-				break;
-			case 15:
-				break;
-			case 16:
-				break;
+			case 9: partie.ajoutSolde(100,partie.getTabJoueurs().get(partie.getIndexJoueurCourant())); break;
+			case 10: partie.ajoutSolde(100,partie.getTabJoueurs().get(partie.getIndexJoueurCourant())); break;
+			case 11: partie.ajoutSolde(50,partie.getTabJoueurs().get(partie.getIndexJoueurCourant())); break;
+			case 12: partie.retraitSolde(10,partie.getTabJoueurs().get(partie.getIndexJoueurCourant())); break; // ou tire carte chance
+			case 13: partie.getTabJoueurs().get(partie.getIndexJoueurCourant()).setNbCarteSortezPrison(partie.getTabJoueurs().get(partie.getIndexJoueurCourant()).getNbCarteSortezPrison() + 1); break;
+			case 14: partie.ajoutSolde(20,partie.getTabJoueurs().get(partie.getIndexJoueurCourant())); break;
+			case 15: partie.ajoutSolde(10,partie.getTabJoueurs().get(partie.getIndexJoueurCourant())); break;
+			case 16: partie.allerA(0); break;
 		}
 	}
 }
